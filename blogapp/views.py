e070b60b833233e6036da92ece9f93d1b9b6b0a1
Ayush <siayush.gupta@gmail.com>
2023-06-01 17:46:11 +0530

    bootstrap added

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Ayush',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Jon',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2023'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blogapp/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogapp/home.html', context)
+
 
 def about(request):
-    return render(request, 'blogapp/about.html')
+    return render(request, 'blogapp/about.html', {'title': 'About'})
