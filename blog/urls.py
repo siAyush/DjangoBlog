158f21ae8996d8902c65367c7220571d9b6f11a2
Ayush <siayush.gupta@gmail.com>
2023-06-02 23:56:02 +0530

    user register

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
+    path('register/', user_view.register, name='register'),
 ]
