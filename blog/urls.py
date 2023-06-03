7074471df75f3fef110e3f67741c6ab261f46c49
Ayush <siayush.gupta@gmail.com>
2023-06-03 16:49:24 +0530

    user profile

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
     path('', include('users.urls')),
