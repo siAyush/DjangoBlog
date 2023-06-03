fc370e75d5c1b086a6e46eeefff9f53ff23f4ec0
Ayush <siayush.gupta@gmail.com>
2023-06-03 15:02:46 +0530

    user url updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
-    path('register/', user_view.register, name='register'),
+    path('', include('users.urls')),
 ]
