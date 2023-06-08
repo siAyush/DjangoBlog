6af0ba4daeae1ad1d46cbdc26638756fa248feef
Ayush <siayush.gupta@gmail.com>
2023-06-08 18:00:20 +0530

    post update

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
-from .views import home, about
+from .views import about
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', about, name='blog-about'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
